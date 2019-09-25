0a26169b4cf8facc5d30c55ee0f14402586b2df9
dengyi <2540628013@qq.com>
2019-09-25 13:51:15 +0800

    2019年9月25日 13:51:12

---
+++ src/adapter_适配器模式/DualPin.java
@@ -0,0 +1,9 @@
+package adapter_适配器模式;
+
+/**
+ * 双插接口
+ */
+public interface DualPin {
+    //
+    public void electrify(int l, int n);//这里没有地线
+}
---
+++ src/adapter_适配器模式/TriplePin.java
@@ -0,0 +1,13 @@
+package adapter_适配器模式;
+
+public interface TriplePin {
+    /**
+     * @param l 火线live
+     * @param n 零线null
+     * @param e 地线earth
+     */
+    /*
+     *
+     */
+    public void electrify(int l, int n, int e);
+}
